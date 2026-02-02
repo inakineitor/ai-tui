8a5df783e16e4a4e5cc4d4d3763d2eb2d6284e09
Iñaki Arango <arangoinaki@gmail.com>
2026-02-02 16:12:28 -0500

    renamed file casing

--- packages/core/src/index.tsx
+++ packages/core/src/index.tsx
@@ -1,3 +1,3 @@
 /** biome-ignore-all lint/performance/noBarrelFile: It's the central export of a library so it is okay. */
 
-export { Button, type ButtonProps } from "./button-1.js";
+export { Button, type ButtonProps } from "./button.js";
