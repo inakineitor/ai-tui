ab8dd0abd263002dd328acd1955cdfa81c4dc0ad
Iñaki Arango <arangoinaki@gmail.com>
2026-02-02 21:57:27 -0500

    removed components package

--- packages/components/src/index.tsx
+++
@@ -1,5 +0,0 @@
-/** biome-ignore-all lint/performance/noBarrelFile: It's the central export of a library so it is okay. */
-
-export { toSlug } from "./to-slug.js";
-export { useIsomorphicLayoutEffect } from "./use-isomorphic-layout-effect.js";
-export { usePrevious } from "./use-previous.js";
